d3eeccff57e33b35fe6c4204af1726bdec4d5e6a
Danielson Alencar <danielson150@gmail.com>
2019-01-03 23:34:11 -0300

    Questao 1 trabalho Django

---
+++ core/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from core.models import Despesa
+
+
+class DespesaAdmin(admin.ModelAdmin):
+    list_display = (
+        'data_criacao', 'tipo_despesa', 'descricao', 'vencimento', 'forma_pagamento', 'quitado', 'conta_prox_vencimento'
+    )
+    list_filter = ('vencimento','quitado',)
+
+    def conta_prox_vencimento(self, obj):
+        return obj.vencimento.strftime('%d/%m/%Y')
+
+    conta_prox_vencimento.short_description = 'Próximo a vencer?'
+    #conta_prox_vencimento.boolean = True
+
+
+admin.site.register(Despesa, DespesaAdmin)
+
+admin.site.site_header = 'Painel de Controle'
+admin.site.index_title = 'Despesas de Ana'
+admin.site.site_title = 'Despesas de Ana'
+
+
